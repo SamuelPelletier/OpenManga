7b1d345e3fc1d127a9109c4bf3742cfe5146cd8b
PELLETIER Samuel <pellesam.info@gmail.com>
2019-05-27 23:15:13 +0200

    Remove sass

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,13 @@ Encore
     .autoProvidejQuery()
 
     .autoProvideVariables({
-        "window.Bloodhound": require.resolve('bloodhound-js'),
-        "jQuery.tagsinput": "bootstrap-tagsinput",
+        "window.Bloodhound": require.resolve('bloodhound-js')
     })
-    .enableSassLoader()
     .enableVersioning()
     .addEntry('js/app', './assets/js/app.js')
     .addEntry('js/login', './assets/js/login.js')
     .addEntry('js/show', './assets/js/show.js')
-    .addStyleEntry('css/app', ['./assets/scss/app.scss', './assets/css/index.css', './assets/css/neon-color.css'])
-    .addStyleEntry('css/admin', ['./assets/scss/admin.scss'])
+    .addStyleEntry('css/app', ['./assets/css/app.css', './assets/css/index.css', './assets/css/neon-color.css'])
     .addStyleEntry('css/show', './assets/css/show.css')
     .addStyleEntry('css/disclaimer', './assets/css/disclaimer.css')
     .addStyleEntry('css/about', './assets/css/about.css')
