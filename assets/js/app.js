074c2433149633cecf1ec32d7c5ab16b32c7202f
PELLETIER Samuel <pellesam.info@gmail.com>
2019-10-17 22:28:49 +0200

    Change pagination for responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -71,5 +71,9 @@ $(function () {
         pages = undefined;
         newUrl = undefined;
     });
+
+    var paginatorActive = $('li[class=active]');
+    paginatorActive.prev().addClass('d-inline');
+    paginatorActive.next().addClass('d-inline');
 });
 
