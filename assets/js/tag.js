7428847636644cb2895d5b8e0053583b7fa0c3c1
PELLETIER Samuel <pellesam.info@gmail.com>
2019-11-20 15:18:02 +0100

    Fix search pagniation and search tag in tag page

--- assets/js/tag.js
+++ assets/js/tag.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $('.letter-list li').on('click', function () {
         var text = $(this).text().trim();
-        if (text == 'All') {
+        if ($(this).hasClass("all")) {
             $('.d-none').removeClass('d-none');
-        } else if (text == 'Other') {
+        } else if ($(this).hasClass("other")) {
             $('.tag-list span a').each(function (index, value) {
                 if ($.inArray($(value).text().trim().toUpperCase()[0], ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']) != -1) {
                     $(value).parent().addClass('d-none');
