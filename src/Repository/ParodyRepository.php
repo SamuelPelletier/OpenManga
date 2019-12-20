8e7072de64bf695869df5bc8f287c8bee2b83bea
PELLETIER Samuel <pellesam.info@gmail.com>
2019-12-20 12:27:00 +0100

    Symfony 4.3 -> 5.0

--- src/Repository/ParodyRepository.php
+++ src/Repository/ParodyRepository.php
@@ -13,7 +13,7 @@ namespace App\Repository;
 
 use App\Entity\Parody;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 class ParodyRepository extends ServiceEntityRepository
 {
