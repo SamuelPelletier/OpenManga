5efee7fa1aadfb7dae1df26d670ac715c0cb535d
Samuel Pelletier <sam@MacBook-Air-de-Samuel.local>
2022-05-06 21:29:07 +0200

    Migration 22

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
-var Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
 
 Encore
     .setOutputPath('public/build/')
@@ -6,12 +12,45 @@ Encore
     .cleanupOutputBeforeBuild()
     .autoProvidejQuery()
 
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    /*
+     * ENTRY CONFIG
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('app', './assets/app.js')
+
+    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    .enableStimulusBridge('./assets/controllers.json')
+
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    //.enableIntegrityHashes(Encore.isProduction())
+
+
     .autoProvideVariables({
         "window.Bloodhound": require.resolve('bloodhound-js')
     })
-    .enableVersioning()
     .enableSassLoader()
-    .addEntry('js/app', ['./assets/js/app.js', './node_modules/@fortawesome/fontawesome-free/js/all.js'])
+    //.addEntry('js/app', ['./assets/js/app.js', './node_modules/@fortawesome/fontawesome-free/js/all.js'])
     .addEntry('js/show', './assets/js/show.js')
     .addEntry('js/tag', './assets/js/tag.js')
     .addEntry('js/form', './assets/js/form.js')
