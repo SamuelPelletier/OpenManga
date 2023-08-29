f9c59e1febc6f0085a53d5ae238e57b7ecb6e15a
Basile Bron <basile.bron@protonmail.com>
2023-08-29 09:20:42 +0200

    populate proxy + misc

--- src/Command/ImportOldMangaCommand.php
+++ src/Command/ImportOldMangaCommand.php
@@ -23,6 +23,7 @@ use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Finder\Finder;
 use WebPConvert\WebPConvert;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 #[AsCommand(
     name: 'app:import-old-manga',
@@ -32,12 +33,15 @@ class ImportOldMangaCommand extends Command
 {
     private $em;
     private $logger;
+    private $cache;
 
     public function __construct(EntityManagerInterface $em, LoggerInterface $logger)
     {
         parent::__construct();
         $this->em = $em;
         $this->logger = $logger;
+        $this->cache = new FilesystemAdapter();
+
     }
 
     protected function configure(): void
@@ -50,29 +54,56 @@ class ImportOldMangaCommand extends Command
         //user output command line
         $io = new SymfonyStyle($input, $output);
 
-        // Get the next page number
         // Create a cache adapter
         $cache = new FilesystemAdapter();
-        
-        // Try to retrieve the integer value from the cache
-        
-        $cacheItem = $cache->getItem('next_page_to_scrap');
-        if ($cacheItem->isHit()) {
-            $nextPageNumber = $cacheItem->get();
-        } else {
-            $nextPageNumber = 1;
-            $cacheItem->set($nextPageNumber);
-            $cache->save($cacheItem);
+
+        $this->PopulateProxyList();
+
+        //Get the list of links to scrap
+        $cacheItem = $this->cache->getItem('link_list');
+
+        if (!$cacheItem->isHit() ) {
+            echo "# POPULATING LINK LIST\n";
+            
+            //TODO save number
+            $nextPageNumber = 285;
+            
+            // Load and set your proxy list (e.g., from a file, database, or API)
+            $linkList = $this->PopulateLinkList($nextPageNumber);
+
+            echo "# found :". count($linkList);
+
+            // Store the proxy list in the cache with a TTL of 1 hour
+            $cacheItem->set($linkList);
+            $this->cache->save($cacheItem);
+        }
+
+        // Retrieve the proxy list from the cache
+        $links = $cacheItem->get();
+
+        for ($i = count($links); $i > 0; $i--) {
+            $io->note(sprintf('downloading :'. $links[$i - 1]));
+            //DEBUG ! TO REMOVE
+            $this->downloadManga($links[$i - 1]);
+            //$this->CallAPI("POST", $_ENV['API_URL']);
+            //END DEBUG
+            $io->success($links[$i - 1], ' has been downloaded');
+            echo("waiting to avoid too much web request");
+            sleep(15);
         }
         
+        return Command::SUCCESS;
+    }    
 
-
-        $url = $_ENV['API_OLD_MANGA_SEARCH'] . $nextPageNumber ;
+    private function PopulateLinkList($nextPageNumber) {
         
+        $url = $_ENV['API_OLD_MANGA_SEARCH'] . $nextPageNumber ;
+
         // Fetch the webpage content
         $html = file_get_contents($url);
 
         if ($html === false) {
+            //TODO USE PROXY TO FETCH LIST
             echo "Failed to fetch the webpage.";
             exit;
         }
@@ -82,9 +113,10 @@ class ImportOldMangaCommand extends Command
         preg_match_all($pattern, $html, $matches, PREG_SET_ORDER);
 
         if (!empty($matches)) {
-            //$nextNmber = max(array_column($matches, 2));
+            print_r($matches);
+            $nextNmber = max(array_column($matches, 2));
             //DEBUG ! TO REMOVE
-            $nextNmber = $nextPageNumber +1;
+            //$nextNmber = $nextPageNumber +1;
                 
             // Update the next page number in the config file
             echo "next page : $nextNmber\n";
@@ -92,40 +124,142 @@ class ImportOldMangaCommand extends Command
             foreach ($matches as $match) {
                 $links[] = $match[1];
             }
-                        
-            // Now you can use the $links array to access the links
-            foreach ($links as $link) {
-                echo $link . "\n";
-
-                for ($i = count($links); $i > 0; $i--) {
-                    $io->note(sprintf('downloading :', $links[$i - 1]));
-                    //DEBUG ! TO REMOVE
-                    //$this->downloadManga($links[$i - 1]);
-
-                    $this->CallAPI("POST", $_ENV['API_URL'], '{
-                        "method": "gdata",
-                        "gidlist": [
-                            [' . 'debug' . ',"' . 'debug' . '"]
-                        ],
-                        "namespace": 1
-                      }');
-                    //END DEBUG
-                    $io->success($links[$i - 1], ' has been downloaded');
-                    sleep(144);
-                }
-            }
         } else {
             $io->error('No links found.');
+            echo 'No links found.';
             exit;
         }
+        return $links;
+    }
+    
+    private function PopulateProxyList() {
+        echo "# POPULATING PROXY LIST\n";
+
+        //start curl
+        $curl = curl_init();
         
-        return Command::SUCCESS;
-    }    
+        //basic
+        $url = $_ENV['PROXY_PROVIDER'];
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_HEADER, 1);
+        // Set a custom user agent string
+        $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36';
+        curl_setopt($curl, CURLOPT_USERAGENT, $userAgent);
+        //ssl
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        //DEBUG
+        //curl_setopt($curl, CURLOPT_VERBOSE, true);
+        
+        // Execute cURL
+        $result = curl_exec($curl);
+
+        // Check for cURL errors
+        if(curl_errno($curl)) {
+            echo 'Curl error: ' . curl_error($curl);
+            exit;
+        }
+
+        // Use regex to extract IP and port information
+        $pattern = $_ENV['PROXY_REGEX_SCRAP'];
+        $matches = [];
+
+        
+        // Close cURL session
+        curl_close($curl);
+        
+        // Output the response
+        echo "#END POPULATING PROXY LIST\n";
+        
+        if (preg_match_all($pattern, $result, $matches)) {
+            $ips = $matches[1];
+            $ports = $matches[2];
+        
+            $proxies = [];
+            foreach ($ips as $index => $ip) {
+                $proxy = $ip . ':' . $ports[$index];
+                $proxies[] = $proxy;
+            }
+        
+            // $proxies array now contains IP:port combinations
+            print_r($proxies);
+            return $proxies;
+        
+        } else {
+            echo "Proxy list not found.";
+            exit;
+        }
+        //re execute the call requested but with the new proxylist
+        //CallAPI($method, $url, $data, $proxyList);
+    }
 
     private function CallAPI($method=null, $url, $data = false)
     {
+        //start curl
         $curl = curl_init();
         
+        //basic
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        // Set a custom user agent string
+        $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36';
+        curl_setopt($curl, CURLOPT_USERAGENT, $userAgent);
+        //ssl
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        //DEBUG
+        curl_setopt($curl, CURLOPT_VERBOSE, true);
+
+        // Fetch the proxy list from cache if available, or create a new one
+        $cacheItem = $this->cache->getItem('proxy_list');
+        if (!$cacheItem->isHit()) {
+            echo "# POPULATING PROXY LIST\n";
+
+            // Load and set your proxy list (e.g., from a file, database, or API)
+            $proxyList = $this->PopulateProxyList();
+
+            // Store the proxy list in the cache with a TTL of 1 hour
+            $cacheItem->set($proxyList);
+            $cacheItem->expiresAfter(500); // 1 hour
+            $this->cache->save($cacheItem);
+        }
+
+        // Retrieve the proxy list from the cache
+        $proxyList = $cacheItem->get();
+
+        //get first value of list and remove it from the array
+        $selectedProxy = array_shift($proxyList);
+        //echo count($proxyList)." proxies. ";
+
+        if($selectedProxy === null) {
+            echo "# POPULATING PROXY LIST\n";
+
+            // Load and set your proxy list (e.g., from a file, database, or API)
+            $proxyList = $this->PopulateProxyList();
+
+            // Store the proxy list in the cache with a TTL of 1 hour
+            $cacheItem->set($proxyList);
+            $cacheItem->expiresAfter(500);
+            $this->cache->save($cacheItem);
+        }
+
+        // Update the cached proxy list with the modified version and the original TTL
+        $cacheItem->set($proxyList);
+        $this->cache->save($cacheItem);
+        //Proxy
+        curl_setopt($curl, CURLOPT_PROXY, $selectedProxy);
+        curl_setopt($curl, CURLOPT_HTTPPROXYTUNNEL, 1);
+        curl_setopt($curl, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+        //curl_setopt($ch, CURLOPT_PROXYUSERPWD, "$proxyUser:$proxyPass");
+
+        //custom time out
+        //curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+
+        //method
         if($method) {
             switch ($method) {
                 case "POST":
@@ -149,59 +283,44 @@ class ImportOldMangaCommand extends Command
             )
         );
         }
-        
-        $proxyList = [
-            //'173.212.195.139:80',
-            //'185.49.170.20:43626',
-            //'50.217.29.198:80',
-            '8.219.97.248:80',
-        ];
-        
-        $randomProxy = $proxyList[array_rand($proxyList)];
-        /*
 
-        $oldIp = $this->GetIp(); //get ip without proxy
-        echo $oldIp;
-        echo "#";
-        
-        $newIp = $this->GetIp($randomProxy); //get ip with proxy
-        echo $newIp;
-        */
-
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($curl, CURLOPT_HEADER, 1);
-
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($curl, CURLOPT_PROXY, $randomProxy);
-
-        curl_setopt($curl, CURLOPT_HTTPPROXYTUNNEL, 1);
-        curl_setopt($curl, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
-        //DEBUG
-        curl_setopt($curl, CURLOPT_VERBOSE, true);
-
-        
-        // Set a custom user agent string
-        $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36';
-        curl_setopt($curl, CURLOPT_USERAGENT, $userAgent);
-        //curl_setopt($ch, CURLOPT_PROXYUSERPWD, "$proxyUser:$proxyPass");
-
-        // Execute cURL session and get the response
+        // Execute cURL
+        echo "# curling ". $url . " using proxy ".$selectedProxy."\n";
         $result = curl_exec($curl);
 
         // Check for cURL errors
         if(curl_errno($curl)) {
-            echo 'Curl error: ' . curl_error($curl);
-            exit;
-        }
+            echo '# Curl error: ' . curl_error($curl)."\n";
+            
+            // Close cURL session
+            curl_close($curl);
         
+            echo "# retry\n";
+            $result = $this->CallAPI($method, $url, $data);
+        } else {
+            // Get the HTTP response code
+            $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        
+            // Handle the response based on the HTTP status code
+            if ($httpCode == 403) {
+                echo "Access to the resource is forbidden (HTTP 403)";
+                    
+                // Close cURL session
+                curl_close($curl);
+            
+                echo "# retry\n";
+                $result = $this->CallAPI($method, $url, $data);
+                // You can take specific actions for 403 errors here
+            } 
+        }
+
         // Close cURL session
         curl_close($curl);
         
         // Output the response
+        echo "@@@@@@";
         echo $result;
+        echo "@@@@@@";
 
         return $result;
     }
@@ -218,6 +337,7 @@ private function downloadManga($link)
         $mangaId = $explode[4];
         $this->logger->info('Try to import - manga id : ' . $mangaId);
         $token = $explode[5];
+        echo "]]#\n";
         $raw = $this->CallAPI("POST", $_ENV['API_URL'], '{
             "method": "gdata",
             "gidlist": [
@@ -225,10 +345,14 @@ private function downloadManga($link)
             ],
             "namespace": 1
           }');
-        echo $raw;
+        echo "#\n";
+        var_dump($raw);
+        echo "#\n";
+        echo "#\n";
         
-        if (json_decode($raw, true)['gmetadata'][0] === null) {
+        if (!json_decode($raw, true)['gmetadata'][0]) {
             $io->error("Empty page scraping or http error.");
+            echo "Empty page scraping or http error.";
             exit;
         } else {
             $json = json_decode($raw, true)['gmetadata'][0];
@@ -283,6 +407,7 @@ private function downloadManga($link)
                     default:
                         if (in_array($explodeTag[1], explode(',', $_ENV['API_TAG_BLOCKED']))) {
                             $this->logger->info('End of import - tag blocked detected');
+                            echo "# End of import - tag blocked detected";
                             exit;
                         }
 
