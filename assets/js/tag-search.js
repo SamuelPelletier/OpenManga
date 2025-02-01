f9c71327e89f384465ff4c8bf23f4336f5f6af51
Samuel Pelletier <pellesam.info@gmail.com>
2025-02-01 18:38:37 +0100

    Fix css

--- assets/js/tag-search.js
+++ assets/js/tag-search.js
@@ -5,9 +5,11 @@ const tagsRecommendation = document.querySelector('.search-recommendation-tag');
 searchField.addEventListener('click', () => {
     if (window.innerWidth >= 768) {
         searchRecommendation.style.opacity = '1';
+        searchRecommendation.style.pointerEvents = 'all';
     }
 });
 
 searchField.addEventListener('blur', () => {
     searchRecommendation.style.opacity = '0';
+    searchRecommendation.style.pointerEvents = 'none';
 });
