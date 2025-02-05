9d27bce8736641fdb5d97a3f172eb784229be4ea
Samuel Pelletier <pellesam.info@gmail.com>
2025-02-05 18:41:08 +0100

    Debug in prod 5

--- assets/js/square.js
+++ assets/js/square.js
@@ -68,11 +68,12 @@ document.addEventListener('DOMContentLoaded', async function () {
     document.getElementById('credit-button')?.addEventListener('click', clickEvent);
 
     let previousValue = document.querySelector('.price-select')?.value;
-    if(previousValue) {
-        //document.getElementById('card-payment-form-container').innerHTML = document.getElementById('card-payment-form-container').innerHTML.replace(/(#credits#)/gi, previousValue);
+    if (previousValue) {
+        document.querySelector('#card-payment-form-container .price-text').innerHTML = document.querySelector('#card-payment-form-container .price-text').innerHTML.replace(" " + previousValue.toString() + " credits", " " + event.target.value + " credits");
+        document.querySelector('#card-payment-form-container .star-button').innerHTML = document.querySelector('#card-payment-form-container .star-button').innerHTML.replace(" " + previousValue.toString() + " credits", " " + event.target.value + " credits");
         document.querySelector('.price-select').addEventListener('change', function (event) {
-            //document.querySelector('#card-payment-form-container .price-text').innerHTML = document.querySelector('#card-payment-form-container .price-text').innerHTML.replace(" " + previousValue.toString() + " credits", " " + event.target.value + " credits");
-            //document.querySelector('#card-payment-form-container .star-button').innerHTML = document.querySelector('#card-payment-form-container .star-button').innerHTML.replace(" " + previousValue.toString() + " credits", " " + event.target.value + " credits");
+            document.querySelector('#card-payment-form-container .price-text').innerHTML = document.querySelector('#card-payment-form-container .price-text').innerHTML.replace(" " + previousValue.toString() + " credits", " " + event.target.value + " credits");
+            document.querySelector('#card-payment-form-container .star-button').innerHTML = document.querySelector('#card-payment-form-container .star-button').innerHTML.replace(" " + previousValue.toString() + " credits", " " + event.target.value + " credits");
             previousValue = event.target.value
         });
     }
