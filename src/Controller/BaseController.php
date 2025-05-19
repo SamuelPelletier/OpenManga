6e85fe83b4361028447a543bfd5367c38085e32d
Samuel Pelletier <pellesam.info@gmail.com>
2025-05-19 19:54:04 +0200

    Allow parameter to get json

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -10,7 +10,7 @@ class BaseController extends AbstractController
     protected function render(string $view, array $parameters = [], Response $response = null): Response
     {
         $jsonQueryParameter = ($this->container->get('request_stack')->getCurrentRequest()->headers->get('Content-Type'));
-        if ($jsonQueryParameter === 'application/json') {
+        if ($jsonQueryParameter === 'application/json' || $this->container->get('request_stack')->getCurrentRequest()->query->getBoolean('json')) {
             return $this->json(current($parameters)->getQuery()->getArrayResult());
         }
         return parent::render($view, $parameters, $response);
