53c70457cd81935944f9190b37c00b0276c21286
Samuel Pelletier <pellesam.info@gmail.com>
2025-09-29 19:39:23 +0200

    Add google auth by api

--- src/Controller/GoogleController.php
+++ src/Controller/GoogleController.php
@@ -5,24 +5,33 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Service\PatreonService;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
 use Google_Client;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Patreon\API;
 use Patreon\OAuth;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
 use Symfony\Component\String\ByteString;
 
 class GoogleController extends AbstractController
 {
+    const APP_IOS = '987590718680-q1hck96puauhl8uigdk1ake8ot1lv60s.apps.googleusercontent.com';
+    const APP_ANDROID = '987590718680-r3mgjflib2bfp6tr9ut46de5o8d96kjc.apps.googleusercontent.com';
+    const APP_WEB = '987590718680-2gcv95f8epo9t06478hpp3t37bjjg2jd.apps.googleusercontent.com';
 
     #[Route("/google_login", name: 'google_login')]
-    public function login(Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager, UserAuthenticatorInterface $userAuthenticator, AuthenticatorInterface $authenticator)
+    public function login(Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager, UserAuthenticatorInterface $userAuthenticator, AuthenticatorInterface $authenticator, JWTTokenManagerInterface $JWTTokenManager, RefreshTokenManagerInterface $refreshTokenManager)
     {
-        $client = new Google_Client(['client_id' => '987590718680-2gcv95f8epo9t06478hpp3t37bjjg2jd.apps.googleusercontent.com']);  // Specify the WEB_CLIENT_ID of the app that accesses the backend
+        $platform = $this::{'APP_' . strtoupper($request->get('platform', 'web'))};
+        $client = new Google_Client(['client_id' => $platform]);
         $payload = $client->verifyIdToken($request->get('credential'));
         if ($payload) {
             $userid = $payload['sub'];
@@ -39,6 +48,17 @@ class GoogleController extends AbstractController
                 $user->setPublicName('NewUser' . $user->getId());
                 $entityManager->flush();
                 $userAuthenticator->authenticateUser($user, $authenticator, $request);
+            }
+            $jsonQueryParameter = ($this->container->get('request_stack')->getCurrentRequest()->headers->get('Content-Type'));
+            if ($jsonQueryParameter === 'application/json' || $this->container->get('request_stack')->getCurrentRequest()->query->getBoolean('json')) {
+                $token = $JWTTokenManager->create($user);
+                $refreshToken = $refreshTokenManager->create();
+                $refreshToken->setUsername($user->getUserIdentifier());
+                $refreshToken->setRefreshToken();
+                $refreshToken->setValid((new DateTime())->modify('+1 month'));
+
+                $refreshTokenManager->save($refreshToken);
+                return new JsonResponse(['token' => $token, 'refresh_token' => $token]);
             }
             return $this->redirectToRoute('user_index');
         }
